dc0afbf74e4ca518060559858fc077287c12e619
Bob <bob@BobdeiMac.local>
2014-07-11 22:18:55 +0800

    add pb

---
+++ Cetris/Client.h
@@ -0,0 +1,13 @@
+//
+//  Client.h
+//  Cetris
+//
+//  Created by Bob  on 14-7-11.
+//  Copyright (c) 2014年 Wan Wei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Client : NSObject
+
+@end
